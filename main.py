7bb79dba306fd29efa8d76361e6d9cd94811cecc
KIMI <plusee@naver.com>
2022-05-25 17:46:53 +0900

    token balance check logic added

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 import time
 
+import numpy
+import pandas as pd
 import requests
 import json
 import datetime
@@ -15,13 +17,46 @@ class covalant:
     opensea_pklay = data["to_opensea_pklay"] # 0xd82dab73fc27c4207f2d87924506a1aca24dc7fc
     opensea_klay = data["to_opensea_klay"] # 0x41cff281b578f4cf45515d6e4efd535e47e76efd
 
-    def get_wallet_balance(self, chain_id, address):
-        endpoint = f'/{chain_id}/address/{address}/balances_v2/?key={self.API_KEY}'
+    def get_wallet_balance(self, address):
+        endpoint = f'/{self.chain_id}/address/{address}/balances_v2/?nft=true&no-nft-fetch=true&key={self.API_KEY}'
         url = self.base_url + endpoint
-        result = requests.get(url).json()
+        while True:
+            try:
+                result = requests.get(url).json()
+                break
+            except requests.RequestException as e:
+                print(e)
+                print('Try again...')
+                time.sleep(2)
         data = result["data"]
-        print(data)
-        return(data)
+        items = data["items"]
+
+        for item in items:
+            if item["contract_address"] == self.address:
+                balance = item["balance"]
+                return balance
+        return False
+
+    def get_token_holders(self):
+        endpoint = f'/{self.chain_id}/tokens/{self.address}/token_holders/?page-size=2018&key={self.API_KEY}'
+        url = self.base_url + endpoint
+        while True:
+            try:
+                result = requests.get(url).json()
+                break
+            except requests.RequestException as e:
+                print(e)
+                print('Try again...')
+                time.sleep(2)
+        data = result["data"]
+        items = data["items"]
+        with open('./holders.txt', 'w') as f:
+            f.write("address, balance\n")
+            for item in items:
+                address = item["address"]
+                balance = item["balance"]
+                f.write(f'{address},{balance}\n')
+        return pd.read_csv('./holders.txt')
 
     def is_original_owner(self, chain_id, address, token_id):
         endpoint =f'/{chain_id}/tokens/{address}/nft_metadata/{token_id}/?key={self.API_KEY}'
@@ -77,7 +112,7 @@ class covalant:
             try:
                 result = requests.get(url).json()
                 break
-            except requests.RequestException as e:
+            except Exception as e:
                 print(e)
                 print('Try again...')
                 time.sleep(2)
@@ -112,9 +147,9 @@ def notlisted_token_find(start_token_id, end_token_id, reset = False):
 
     if reset == True:
         with open('./notListed log.csv', 'w') as f:
-            pass
+            f.write('condition, token id, block signed at, to address, numbers of log events, total transaction, issued transaction')
         with open('./notListed tokenID.txt', 'w') as f:
-            pass
+            f.write('condition, token id, original owner, owner')
 
     api = covalant()
 
@@ -124,11 +159,11 @@ def notlisted_token_find(start_token_id, end_token_id, reset = False):
             continue
         _notListed, token_id, block_signed_at, to_address, log_events_length, total_transaction, num = api.transaction_check(api.chain_id, api.address, i)
         with open('./notListed log.csv', 'a') as f:
-            f.write(f'{_notListed}, {token_id}, {block_signed_at}, {to_address}, {log_events_length}, {total_transaction}, {num}\n')
+            f.write(f'{_notListed},{token_id},{block_signed_at},{to_address},{log_events_length},{total_transaction},{num}\n')
         if  _notListed == "condition A" or _notListed == "condition B":
             original_owner, owner = api.get_owner(token_id)
             with open('./notListed tokenID.txt', 'a') as f:
-                f.write(f'{_notListed}, {token_id}, {original_owner}, {owner}\n')
+                f.write(f'{_notListed},{token_id},{original_owner},{owner}\n')
         print(i, " done")
 
     ended_time = datetime.datetime.now()
@@ -138,8 +173,34 @@ def notlisted_token_find(start_token_id, end_token_id, reset = False):
     return cost_time
 
 def tier_calculate():
-    with open('./notListed tokenID.txt', 'r') as f:
-        tokenId = f.readline()
-        api = covalant()
+    column_name = ['condition', 'token id', 'original owner', 'owner']
+    data = pd.read_csv('./notListed tokenID - test2.txt').apply(lambda x: x.str.strip(), axis = 1)
+    dataframe = data.values.tolist()
+    new_dataframe = pd.DataFrame(dataframe, columns=column_name)
+    indexNames = new_dataframe[new_dataframe['condition'] == 'condition B'].index
 
-notlisted_token_find(756, 7778)
+    groups = new_dataframe.groupby('owner')
+    new_group = pd.DataFrame(groups.size()).reset_index()
+    new_group.columns = ['address', 'sum_balance']
+    new_group = new_group.set_index('address')
+    new_group["status"] = '만족'
+    new_group.to_csv('test.txt')
+    new_group = new_group.applymap(str)
+    api = covalant()
+    holder = api.get_token_holders().set_index('address')
+    # new_balance = pd.merge(holder, new_group, how='outer', on='address')
+    new_balance = holder.join(new_group)
+
+    new_dataframe.drop(indexNames, inplace=True)
+    groupA = new_dataframe.groupby('owner')
+    new_group = pd.DataFrame(groupA.size()).reset_index()
+    new_group.columns = ['address', 'A_balance']
+    new_group = new_group.set_index('address')
+    new_group["A condition"] = 'A 만족'
+    new_group = new_group.applymap(str)
+    new_balance = new_balance.join(new_group)
+    new_balance.to_csv('final_balance.csv')
+
+tier_calculate()
+
+# notlisted_token_find(1207, 1208)
